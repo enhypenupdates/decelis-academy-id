2d79c0d15c74adb818437b3fb0370c822961edf8
ENHYPEN UPDATES <contactenhypenupdates@gmail.com>
2024-05-12 03:42:13 +0800

    main.js

--- main.js
+++ main.js
@@ -190,7 +190,7 @@ document.querySelector('#image-input').addEventListener('change', function () {
 });
 
 /*-- Auto Format Birthday ------*/
-var cleave = new Cleave('.registrationDate', {
+var cleave = new Cleave('.regDate', {
 	date: true,
 	delimiter: '-',
 	datePattern: ['M', 'd', 'Y'],
